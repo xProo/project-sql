53ace930ca2d9511e267feaa62a804ae47254bb0
SoSo Pro <c-sohane@hotmail.fr>
2024-10-31 18:11:29 +0100

    [added] added zoneTarifaire table and station table

--- init_database.sql
+++ init_database.sql
@@ -7,3 +7,18 @@ CREATE TABLE Transport (
     duree_trajet INT CHECK (duree_trajet > 0)
 );
 
+-- Table des zones tarifaires
+CREATE TABLE ZoneTarifaire (
+    numero INT PRIMARY KEY,
+    nom VARCHAR(32) NOT NULL,
+    prix DECIMAL(5, 2) CHECK (prix >= 0)
+);
+
+-- Table des stations
+CREATE TABLE Station (
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(64) NOT NULL,
+    commune VARCHAR(32) NOT NULL,
+    zone_numero INT,
+    FOREIGN KEY (zone_numero) REFERENCES ZoneTarifaire(numero)
+);
